04b8c4aab2adee6b3c69778fdc4e6486134df1ba
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-12-09 17:37:41 +0100

    Formatted formhandler indentation.

--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -1,35 +1,35 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
-	$title = trim($_POST['title']);
-	$dueDate = trim($_POST['due-date']);
-	$description = trim($_POST['description']);
+    $title = trim($_POST['title']);
+    $dueDate = trim($_POST['due-date']);
+    $description = trim($_POST['description']);
 
-	if (empty($title) || empty($dueDate) || empty($description)) {
-		die("All fields are required.");
-	}
+    if (empty($title) || empty($dueDate) || empty($description)) {
+        die("All fields are required.");
+    }
 
-	try {
-		require_once 'dbh.inc.php';
+    try {
+        require_once 'dbh.inc.php';
 
-		// SQL query with placeholders
-		$query = 'INSERT INTO tasks (title, due_date, `description`) VALUES (?, ?, ?);';
+        // SQL query with placeholders
+        $query = 'INSERT INTO tasks (title, due_date, `description`) VALUES (?, ?, ?);';
 
-		// Prepare the SQL statement
-		$stmt = $pdo->prepare($query);
+        // Prepare the SQL statement
+        $stmt = $pdo->prepare($query);
 
-		// Bind values to the statement
-		$stmt->execute([$title, $dueDate, $description]);
+        // Bind values to the statement
+        $stmt->execute([$title, $dueDate, $description]);
 
-		$pdo = null;
-		$stmt = null;
+        $pdo = null;
+        $stmt = null;
 
-		header('Location: ../index.php');
-		die();
-	}
-	catch (PDOException $e) {
-		die('Query failed: ' . $e->getMessage());
-	}
+        header('Location: ../index.php');
+        die();
+    }
+    catch (PDOException $e) {
+        die('Query failed: ' . $e->getMessage());
+    }
 }
 else {
 	header('Location: ../index.php');
